2dba9203d433fdee1191980fe7826e7618585fba
LinoyHorvat <linoy.horvat@gmail.com>
2022-01-24 15:33:35 +0200

    connit2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const uri = require("../.config/dev");
 
 mongoose.connect(uri.mongoURI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true
 })
